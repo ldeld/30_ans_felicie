aa968346312697bf26ff29a74237eff491c2139d
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2020-05-28 01:52:48 +0200

    rename header to headersection and fix z-index o canvas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <canvas id="confetti-canvas"></canvas>
-    <Header />
+    <HeaderSection />
     <Caroussel />
+    <canvas id="confetti-canvas"></canvas>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import HeaderSection from './components/HeaderSection.vue'
 import Caroussel from './components/Caroussel.vue'
 // Todo: move this to it's own component?
 import ConfettiGenerator from "confetti-js";
@@ -15,14 +15,14 @@ import ConfettiGenerator from "confetti-js";
 export default {
   name: 'App',
   components: {
-    Header,
+    HeaderSection,
     Caroussel
   },
   mounted() {
     window.addEventListener("load", () => {
       const confettiSettings = {"target":"confetti-canvas","max":"220","size":"1","animate":true,"props":["circle","square","triangle","line"],"colors":[[165,104,246],[230,61,135],[0,199,228],[253,214,126]],"clock":"50","rotate":true,"width":"1280","height":"1000","start_from_edge":true,"respawn":false};
       const confetti = new ConfettiGenerator(confettiSettings);
-      confetti.render();
+      setTimeout(() => confetti.render(), 1000)
     })
   }
 }
@@ -49,5 +49,6 @@ body {
   left: 0;
   height: 100vh;
   width: 100vw;
+  z-index: -1;
 }
 </style>
