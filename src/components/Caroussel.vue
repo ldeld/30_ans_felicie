d491d2fb55c6d6194d62514e06b523938eeefe46
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2020-06-07 14:47:16 +0200

    carousels css

--- src/components/Caroussel.vue
+++ src/components/Caroussel.vue
@@ -10,6 +10,8 @@
       <div class="slide">
         <img src="https://source.unsplash.com/random" alt="">
       </div>
+      <template slot="prevButton"><i class="fas fa-chevron-left"></i></template>
+      <template slot="nextButton"><i class="fas fa-chevron-right"></i></template>
     </agile>
   </div>
 </template>
@@ -26,10 +28,42 @@
   }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
   .slide {
     img, iframe {
       height: 60vh;
+    }
+  }
+
+  .agile__nav-button {
+    z-index: 5;
+    margin: -60vh 60px 0 60px;
+    padding: 0 30px;
+    border-radius: 6px;
+    border: none;
+    background-color: transparent;
+    color: #ddd;
+    font-size: 40px;
+
+    transition: 0.2s ease;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.05);
+    }
+  }
+
+  .agile__dot {
+    button {
+      border: none;
+      background-color: #888;
+      width: 40px;
+      height: 3px;
+      margin: 0 10px;
+      border-radius: 2px;
+    }
+
+    &--current button {
+      background-color: #fff;
     }
   }
 </style>
